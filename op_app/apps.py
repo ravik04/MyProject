1f210c615b4afb88c4644d05886ae3bcf792c2e3
Ravi Katariya <97892870+ravik04@users.noreply.github.com>
2024-03-13 19:41:21 +0530

    Add files via upload

---
+++ op_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OpAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'op_app'
---
+++ op_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+
+def index(request):
+     return render(request, 'index.html')
+
+def home(request):
+     return render(request,'home.html')
+
+# Create your views here.
